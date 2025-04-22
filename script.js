6f4aad223140ae06d12df655ccc817630e164099
eatitboiiissss <atriumcopeland0@gmail.com>
2025-04-22 17:11:29 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMembers() {
-  const list = document.getElementById('memberList');
-  list.style.display = list.style.display === 'block' ? 'none' : 'block';
-}
-
-function clickMember(name) {
-  alert(`You clicked on: ${name}`);
-}
